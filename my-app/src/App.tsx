8d1d83e3d6bf245d211f93a06b2b9fc15093cb52
ShuheiKurinami <zhoupinglibo252@gmail.com>
2025-01-29 12:07:13 +0900

    フロントエンドサイド修正

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./views/user/UserList";
+import UserForm from "./views/user/UserForm";
+import GenreList from "./views/genre/GenreList";
+import GenreForm from "./views/genre/GenreForm";
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/users" element={<UserList />} />
+        <Route path="/users/new" element={<UserForm />} />
+        <Route path="/genres" element={<GenreList />} />
+        <Route path="/genres/new" element={<GenreForm />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
